977a06c4bef99dd69274e0133097dffaa18ff285
crwhite14 <crwhite@cs.cmu.edu>
2024-06-19 00:00:48 -0700

    make house_traversal judging stricter

--- livebench/process_results/reasoning/house_traversal/utils.py
+++ livebench/process_results/reasoning/house_traversal/utils.py
@@ -12,13 +12,20 @@ def house_traversal_process_results(ground_truth: str, llm_answer: str) -> int:
     last_bold = bold_words[-1][1]
     ground_truth_names = ground_truth.lower().split(" ")
 
-    # check if all the ground truth names are in the last bolded part
-    if all([name in last_bold for name in ground_truth_names]):
-        return 1
-
     # check if all the ground truth names are in the last few bolded words, in order
     if len(bold_words) >= len(ground_truth_names):
         if all([name in last_bold[-1 - i] for i,name in enumerate(ground_truth_names[::-1])]):
             return 1
 
-    return 0
+    # check if all the ground truth names are in the last bolded part, in order
+    last_index = -1
+    for name in ground_truth_names:
+        index = last_bold.find(name)
+        if index == -1:
+            return 0
+        elif index <= last_index:
+            return 0
+        else:
+            last_index = index
+
+    return 1
