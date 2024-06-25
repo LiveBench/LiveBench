b98e0b691f44dff7f5b306d3d8923ff878b09664
crwhite14 <crwhite@cs.cmu.edu>
2024-06-25 15:20:42 -0700

    make web_of_lies_v2 parsing less strict

--- livebench/process_results/reasoning/web_of_lies_v2/utils.py
+++ livebench/process_results/reasoning/web_of_lies_v2/utils.py
@@ -7,25 +7,17 @@ def web_of_lies_process_results(ground_truth: str, llm_answer: str) -> int:
     # pull out words in bold
     bold_words = re.findall(r'\*\*(.*?)\*\*', llm_answer)
 
-    # Check if any bold words were found and compare the last one to the ground_truth
-    if bold_words and bold_words[-1].lower() == ground_truth.lower():
+    if not bold_words:
+        return 0
+
+    last_bold = bold_words[-1].lower()
+
+    # Check if last_bold is an exact match of ground_truth
+    if last_bold == ground_truth.lower():
         return 1
-    else:
-        return 0
 
-def house_traversal_process_results(ground_truth: str, llm_answer: str) -> int:
+    # Check if last_bold contains the ground_truth
+    if last_bold.count("yes") + last_bold.count("no") == 3 and ground_truth.lower() in last_bold:
+        return 1        
 
-    # pull out words in bold
-    bold_words = re.findall(r'\*\*(.*?)\*\*', llm_answer.lower())
-    if not len(bold_words):
-        return 0
-
-    last_bold = bold_words[-1]
-    answers = last_bold.split(", ")
-    gt_answers = ground_truth.lower().split(", ")
-
-    if len(answers) < len(gt_answers):
-        return 0
-    if all([answers[i] == gt_answers[i] for i in range(len(gt_answers))]):
-        return 1
     return 0
