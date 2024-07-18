95dbca5980cc0254230d870e9490e8904d100b9a
crwhite14 <crwhite@cs.cmu.edu>
2024-07-18 00:19:10 +0000

    fix bug in gen_ground_truth_judgment

--- livebench/gen_ground_truth_judgment.py
+++ livebench/gen_ground_truth_judgment.py
@@ -21,7 +21,6 @@ from livebench.process_results.data_analysis.tablejoin.utils import joinmap_proc
 from livebench.process_results.reasoning.web_of_lies_v2.utils import web_of_lies_process_results
 from livebench.process_results.reasoning.house_traversal.utils import house_traversal_process_results
 from livebench.process_results.reasoning.zebra_puzzle.utils import zebra_puzzle_process_results
-from livebench.process_results.reasoning.spatial.utils import spatial_process_results
 from livebench.process_results.math.math_competitions.utils import mathcontest_process_results,aime_process_results 
 from livebench.process_results.math.olympiad.utils import proof_rearrangement_process_results
 from livebench.process_results.math.AMPS_Hard.utils import amps_hard_process_results 
@@ -108,9 +107,6 @@ def play_a_match_gt(match: MatchSingle, output_file: str):
         category = "reasoning"
     elif task_or_subtask == "zebra_puzzle":
         score = zebra_puzzle_process_results(ground_truth, llm_answer)
-        category = "reasoning"
-    elif task_or_subtask == "spatial":
-        score = spatial_process_results(ground_truth, llm_answer)
         category = "reasoning"
     elif task_or_subtask == 'typos':
         score = typos_process_results(ground_truth, llm_answer)
