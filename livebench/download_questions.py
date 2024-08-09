6f3f1e371212bfbe71de9c9550d52a5ab72c52e8
crwhite14 <crwhite@cs.cmu.edu>
2024-08-08 22:47:27 -0700

    add livebench_releases param in download_questions

--- livebench/download_questions.py
+++ livebench/download_questions.py
@@ -11,7 +11,9 @@ from livebench.common import (
 categories, tasks = get_categories_tasks(LIVE_BENCH_DATA_SUPER_PATH)
 
 for category_name, task_names in tqdm(tasks.items()):
-    questions = load_questions(categories[category_name], task_name=None, begin=None, end=None)
+    # load questions from all livebench releases
+    livebench_releases = set(['2024-07-26', '2024-06-24'])
+    questions = load_questions(categories[category_name], livebench_releases=livebench_releases, task_name=None, begin=None, end=None)
 
     category_path = f"data/{LIVE_BENCH_DATA_SUPER_PATH}/{category_name}"
     question_file_path = f"{category_path}/question.jsonl"
