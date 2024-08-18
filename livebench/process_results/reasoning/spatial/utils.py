ed36110dba44ebc737506f58a428b27df8b00dfc
crwhite14 <crwhite@cs.cmu.edu>
2024-08-17 18:07:05 -0700

    allow boxed output format for spatial reasoning task

--- livebench/process_results/reasoning/spatial/utils.py
+++ livebench/process_results/reasoning/spatial/utils.py
@@ -1,6 +1,5 @@
-
 import re
-
+from typing import Optional
 
 def spatial_process_results(ground_truth: str, llm_answer: str) -> int:
 
@@ -12,6 +11,7 @@ def spatial_process_results(ground_truth: str, llm_answer: str) -> int:
     }
 
     bold_words = re.findall(r'\*\*([^\*]+)\*\*', llm_answer)
+    score = 0
 
     # allow the answer to be within the last 3 bolded words
     words_to_check = []
@@ -21,22 +21,77 @@ def spatial_process_results(ground_truth: str, llm_answer: str) -> int:
 
     for i, word in enumerate(words_to_check):
         if word == ground_truth.strip().lower():
-            return 1
+            score = 1
 
         # allow the answer to be the number spelled out
         if word in word_to_number and word_to_number[word] == ground_truth.strip().lower():
-            return 1
+            score = 1
 
         # allow certain cases like "two tetrahedra" == "tetrahedra" and "equilateral triangle" == "triangle"
         # while still disallowing cases like "circle square triangle" == "triangle"
         for answer in ["tetrahedra", "tetrahedron", "triangle", "square"]:
             if ground_truth.strip().lower() == answer and answer in word and len(word) < (2 * len(answer) + 5):
-                return 1
+                score = 1
+
+    allow_boxed = True
+    if score == 0 and allow_boxed:
+        llm_answer = llm_answer.replace("\\\\fbox{", "\\\\boxed{")
+        last_boxed = last_boxed_only_string(llm_answer)
+        if last_boxed:
+            parsed_answer = remove_boxed(last_boxed)
+            if parsed_answer == ground_truth:
+                score = 1
 
     debug = False
-    if debug and bold_words:
-        print('failed, answer vs ground_truth:', bold_words[-1].strip().lower(), ground_truth.strip().lower())
-        print("failed, full bold:", bold_words)
-    elif debug:
-        print("failed, did not have bolds", llm_answer[-20:], "vs", ground_truth.strip().lower())
-    return 0
+    if debug and score == 0:
+        print("INCORRECT")
+        print("GROUND TRUTH", ground_truth.strip().lower())
+        if bold_words:
+            print("BOLD WORDS:", bold_words[-1].strip().lower())
+        print("END OF OUTPUT", llm_answer[-50:])        
+
+    return score
+
+
+def last_boxed_only_string(string: str) -> Optional[str]:
+    idx = string.rfind("\\boxed")
+    if "\\boxed " in string:
+        return "\\boxed " + string.split("\\boxed ")[-1].split("$")[0]
+    if idx < 0:
+        idx = string.rfind("\\fbox")
+        if idx < 0:
+            return None
+
+    i = idx
+    right_brace_idx = None
+    num_left_braces_open = 0
+    while i < len(string):
+        if string[i] == "{":
+            num_left_braces_open += 1
+        if string[i] == "}":
+            num_left_braces_open -= 1
+            if num_left_braces_open == 0:
+                right_brace_idx = i
+                break
+        i += 1
+
+    if right_brace_idx is None:
+        retval = None
+    else:
+        retval = string[idx : right_brace_idx + 1].replace("$", "").replace("fbox","boxed")
+
+    return retval
+
+
+def remove_boxed(s: str) -> str:
+    if "\\boxed " in s:
+        left = "\\boxed "
+        assert s[: len(left)] == left
+        return s[len(left) :]
+
+    left = "\\boxed{"
+
+    assert s[: len(left)] == left
+    assert s[-1] == "}"
+
+    return s[len(left) : -1]
