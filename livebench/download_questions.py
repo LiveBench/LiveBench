ab834ea6532aa042943afc3b228372f88415473d
Gabriel Guralnick <gabriel@abacus.ai>
2024-12-12 16:39:23 -0800

    update livebench releases in download_questions

--- livebench/download_questions.py
+++ livebench/download_questions.py
@@ -12,7 +12,7 @@ categories, tasks = get_categories_tasks(LIVE_BENCH_DATA_SUPER_PATH)
 
 for category_name, task_names in tqdm(tasks.items()):
     # load questions from all livebench releases
-    livebench_releases = set(['2024-07-26', '2024-06-24'])
+    livebench_releases = set(['2024-07-26', '2024-06-24', '2024-08-31'])
     questions = load_questions(categories[category_name], livebench_releases=livebench_releases, task_name=None, begin=None, end=None)
 
     category_path = f"data/{LIVE_BENCH_DATA_SUPER_PATH}/{category_name}"
