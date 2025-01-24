b0b02c7dec766f7960282897a12cfba72e1f11b3
Gabriel Guralnick <gabriel@abacus.ai>
2025-01-24 13:40:56 -0800

    log error stack traces

--- livebench/model/completions.py
+++ livebench/model/completions.py
@@ -162,6 +162,7 @@ def chat_completion_aws(model, conv, temperature, max_tokens, api_dict=None) -> 
             "temperature": temperature,
         },
     )
+
     output = response["output"]["message"]["content"][0]["text"]
     num_tokens = response["usage"]["output"]["totalTokens"]
 
